4488ea7507225b068ea437c85851440073260ace
Enrico Stara <enrico.stara@gmail.com>
2014-11-24 15:33:58 +0100

    Add `time` module test

---
+++ test/time.test.js
@@ -0,0 +1,16 @@
+require('should');
+var time = require('../lib/time');
+
+describe('time', function () {
+
+    describe('#getLocalTime()', function () {
+        it('should be a synchronized time', function () {
+            var offset = 1000;
+            var duration = 200;
+            var localTime = new Date().getTime();
+            var serverTime = localTime + offset;
+            time.timeSynchronization(serverTime, duration);
+            time.getLocalTime().should.be.approximately(localTime + (offset + duration/2), 3);
+        })
+    });
+});
