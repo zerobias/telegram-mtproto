6a69232d77182a733449fbbfecdca915e7d4a925
Enrico Stara <enrico.stara@gmail.com>
2014-12-04 18:47:16 +0100

    Add the EncryptedMessage class, not working version

---
+++ test/message/encrypted-message.test.js
@@ -0,0 +1,23 @@
+require('should');
+var EncryptedMessage = require('../../lib/message/encrypted-message');
+
+describe('EncryptedMessage', function () {
+
+    describe('#init()', function () {
+        it('should return an instance', function () {
+
+        })
+    });
+
+    describe('#serialize()', function () {
+        it('should serialize the msg', function () {
+
+        })
+    });
+
+    describe('#deserialize()', function () {
+        it('should de-serialize the msg', function () {
+
+        })
+    });
+});
