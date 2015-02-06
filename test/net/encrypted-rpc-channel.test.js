0cc0d7c4ee16ffcb0ba462418e52fa63d61f1e85
Enrico Stara <enrico.stara@gmail.com>
2015-02-06 14:32:56 +0100

    Initialise the api connection wrapping the first call with 'invokeWithLayer' and 'initConnection'

--- test/net/encrypted-rpc-channel.test.js
+++ test/net/encrypted-rpc-channel.test.js
@@ -33,16 +33,6 @@ describe('EncryptedRpcChannel', function() {
         "type": "auth.SentCode"
     }).getType();
 
-    var sendCode = new SendCode({
-        props: {
-            phone_number: '003934987654321',
-            sms_type: 5,
-            api_id: 10534,
-            api_hash: '844584f2b1fd2daecee726166dcc1ef8',
-            lang_code: 'it'
-        }
-    });
-
     var authKey = new auth.AuthKey(
         new Buffer('4efd920588a83ac9', 'hex'),
         new Buffer('33843a2caeb2452873c89349fd8f7221ba734c8df5e05c122a9e36b036995afc81057270b71b6375ae1ccf59514519748bbb0ec508dde9d17bce70e3dcf685b3731917b144710233f0d4b6a1d69b37722eaa075c5aeabde8d1848ef04af661055f52ab406ab7df8dec44a7ddcbf9de5be36ace00ecfbf5cb69b156c18a469e70658b99ecc2d1ff8de47cf573589b475833016a327f855965b7f541a9926651e50e4dcea69ebb5adbe2cc6ad49b36d38c6058c9f1fff06b07688ccb129e20aef00d77912a2f61cb82274fa86d0aa24b58c9e12cad8c751c6da2831ec635d053bd99b26a6fc83a28c64ff5c94efb5ef82356783c6f2ca0b55a074de82de553c6cb', 'hex')
@@ -76,13 +66,10 @@ describe('EncryptedRpcChannel', function() {
 
     describe('#init()', function() {
         it('should create an instance', function() {
-            var rpcChannel = new net.EncryptedRpcChannel(tcpConn, authKey, '0x77907373a54aba77', '0xfce2ec8fa401b366');
+            var rpcChannel = new net.EncryptedRpcChannel(tcpConn);
             rpcChannel.should.be.ok;
             rpcChannel.should.have.properties({
-                _connection: tcpConn,
-                _authKey: authKey,
-                _sessionId: '0x77907373a54aba77',
-                _serverSalt: '0xfce2ec8fa401b366'
+                _connection: tcpConn
             });
             rpcChannel.isOpen().should.be.true;
         })
@@ -97,15 +84,28 @@ describe('EncryptedRpcChannel', function() {
         })
     });
 
-    describe('#close()', function() {
+    net.EncryptedRpcChannel.prototype._deserializeResponse = function(response, context) {
+        return new message.EncryptedMessage({buffer: response, authKey: context.authKey}).deserialize(true).body;
+    };
+
+    describe('#callMethod()', function() {
         it('should call the method', function(done) {
-
-            net.EncryptedRpcChannel.prototype._deserializeResponse = function (response) {
-                return new message.EncryptedMessage({buffer: response, authKey: this._authKey}).deserialize(true).body;
-            };
-            var rpcChannel = new net.EncryptedRpcChannel(tcpConn, authKey, '0x77907373a54aba77', '0xfce2ec8fa401b366');
-
-            rpcChannel.callMethod(sendCode, 0, function(ex, resObj, duration) {
+            var sendCode = new SendCode({
+                props: {
+                    phone_number: '003934987654321',
+                    sms_type: 5,
+                    api_id: 10534,
+                    api_hash: '844584f2b1fd2daecee726166dcc1ef8',
+                    lang_code: 'it'
+                }
+            });
+            var rpcChannel = new net.EncryptedRpcChannel(tcpConn);
+            rpcChannel.callMethod(sendCode, {
+                authKey: authKey,
+                serverSalt: '0xfce2ec8fa401b366',
+                sessionId: '0x77907373a54aba77',
+                sequenceNumber: 0
+            }, function(ex, resObj, duration) {
                 if (ex) {
                     console.log(ex);
                 }
@@ -117,10 +117,57 @@ describe('EncryptedRpcChannel', function() {
                     api_hash: '844584f2b1fd2daecee726166dcc1ef8',
                     lang_code: 'it'
                 });
-
                 console.log('calling the method takes %sms', duration);
                 done();
+            });
+        })
+    });
 
+    describe('#callMethod()', function() {
+        it('should call the wrapped method', function(done) {
+            var sendCode = new SendCode({
+                props: {
+                    phone_number: '003934987654321',
+                    sms_type: 5,
+                    api_id: 10534,
+                    api_hash: '844584f2b1fd2daecee726166dcc1ef8',
+                    lang_code: 'it'
+                }
+            });
+            var rpcChannel = new net.EncryptedRpcChannel(
+                tcpConn, {
+                    appId: 1234,
+                    appVersion: '1.0.0'
+                }
+            );
+            rpcChannel.callMethod(sendCode, {
+                authKey: authKey,
+                serverSalt: '0xfce2ec8fa401b366',
+                sessionId: '0x77907373a54aba77',
+                sequenceNumber: 0
+            }, function(ex, resObj, duration) {
+                if (ex) {
+                    console.log(ex);
+                }
+                resObj.should.be.ok;
+                resObj.should.have.properties({
+                    layer: 23
+                });
+                var initConn = new mtproto.service.initConnection.Type({buffer: resObj.query}).deserialize();
+                initConn.should.have.properties({
+                    api_id: 1234,
+                    app_version: '1.0.0'
+                });
+                var resObj = new SendCode({buffer: initConn.query}).deserialize();
+                resObj.should.have.properties({
+                    phone_number: '003934987654321',
+                    sms_type: 5,
+                    api_id: 10534,
+                    api_hash: '844584f2b1fd2daecee726166dcc1ef8',
+                    lang_code: 'it'
+                });
+                console.log('calling the method takes %sms', duration);
+                done();
             });
         })
     });
