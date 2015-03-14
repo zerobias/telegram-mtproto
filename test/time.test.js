a38960751ec00d9b27128b7eb1949506e7d40615
Enrico Stara <enrico.stara@gmail.com>
2015-03-14 20:01:43 +0100

    Change a log message, update the unit-test

--- test/time.test.js
+++ test/time.test.js
@@ -7,7 +7,7 @@ describe('time', function () {
     describe('#getLocalTime()', function () {
         it('should be a synchronized time', function () {
             var offset = 1000;
-            var duration = 200;
+            var duration = 50;
             var localTime = new Date().getTime();
             var serverTime = localTime + offset;
             time.timeSynchronization(serverTime, duration);
