126c6409d358503b76887ab9be7865c5cad0b745
Enrico Stara <enrico.stara@gmail.com>
2015-03-14 20:17:27 +0100

    Update the inner message management in EncryptedMessage, update the unit-test

--- test/message/encrypted-message.test.js
+++ test/message/encrypted-message.test.js
@@ -15,6 +15,18 @@ describe('EncryptedMessage', function() {
         new Buffer('33843a2caeb2452873c89349fd8f7221ba734c8df5e05c122a9e36b036995afc81057270b71b6375ae1ccf59514519748bbb0ec508dde9d17bce70e3dcf685b3731917b144710233f0d4b6a1d69b37722eaa075c5aeabde8d1848ef04af661055f52ab406ab7df8dec44a7ddcbf9de5be36ace00ecfbf5cb69b156c18a469e70658b99ecc2d1ff8de47cf573589b475833016a327f855965b7f541a9926651e50e4dcea69ebb5adbe2cc6ad49b36d38c6058c9f1fff06b07688ccb129e20aef00d77912a2f61cb82274fa86d0aa24b58c9e12cad8c751c6da2831ec635d053bd99b26a6fc83a28c64ff5c94efb5ef82356783c6f2ca0b55a074de82de553c6cb', 'hex')
     );
 
+    var Body = new tl.TypeBuilder('namespace', {
+        "id": "66666",
+        "predicate": "body",
+        "params": [
+            {
+                "name": "key",
+                "type": "int"
+            }
+        ],
+        "type": "Body"
+    }).getType();
+
     describe('#init()', function() {
         it('should return an instance', function() {
 
@@ -53,7 +65,11 @@ describe('EncryptedMessage', function() {
     describe('#serialize()', function () {
         it('should serialize the msg', function (done) {
             var msg = new EncryptedMessage({
-                message: 'ffff',
+                message: new Body({
+                    props: {
+                        key: 6666
+                    }
+                }),
                 authKey: authKey,
                 serverSalt: '0xfce2ec8fa401b366',
                 sessionId: '0x77907373a54aba77',
@@ -63,7 +79,7 @@ describe('EncryptedMessage', function() {
             var buffer = msg.serialize();
             buffer.should.be.ok;
             console.log(buffer.toString('hex'));
-            buffer.slice(0, -16).toString('hex').should.be.equal('4efd920588a83ac9885bda4c4bde1243252290042cd6e2aff35150888ae03a47d8f8fcebde1afe68d0be2afd7ff559346832f4ed0474cb3c');
+            buffer.slice(0, -16).toString('hex').should.be.equal('4efd920588a83ac9f33f86b0f793bce2fc491b4771739d5ba094e1c361dc2499db7cfddd9c256d882b1240f3e9728765c39ae5f03b69a62d');
             done();
         })
     });
@@ -72,13 +88,14 @@ describe('EncryptedMessage', function() {
         it('should deserialize the msg', function () {
 
             var encMsg = new EncryptedMessage({
-                buffer: new Buffer('4efd920588a83ac9885bda4c4bde1243252290042cd6e2aff35150888ae03a47d8f8fcebde1afe68d0be2afd7ff559346832f4ed0474cb3c72a7364a6ad67534b65b0f75093d5252', 'hex'),
+                buffer: new Buffer('4efd920588a83ac9f33f86b0f793bce2fc491b4771739d5ba094e1c361dc2499db7cfddd9c256d882b1240f3e9728765c39ae5f03b69a62d62bd4e4f577f4ec297c7614438fc2755', 'hex'),
                 authKey: authKey});
             var decrypted = encMsg.deserialize(true);
             decrypted.should.be.ok;
 
             var msg = decrypted;
-            msg.body.toString('hex').should.equal('ffff');
+            msg.body.should.be.an.instanceof(Body);
+            msg.body.key.should.be.equal(6666);
             msg.should.have.properties({
                 serverSalt: '0xfce2ec8fa401b366',
                 sessionId: '0x77907373a54aba77',
@@ -91,8 +108,9 @@ describe('EncryptedMessage', function() {
                 session_id: '0x77907373a54aba77'
             });
             msg.innerMessage.payload.should.be.an.instanceof(mtproto.type.Message);
-            msg.innerMessage.payload.bytes.should.be.equal(2);
-            msg.innerMessage.payload.body.toString('hex').should.equal('ffff');
+            msg.innerMessage.payload.body.should.be.an.instanceof(Body);
+            msg.innerMessage.payload.bytes.should.be.equal(8);
+            msg.innerMessage.payload.body.key.should.be.equal(6666);
         })
     });
 });
