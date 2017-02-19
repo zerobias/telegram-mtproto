545de1e2869e1948b66e175ada5a216468c5b5bd
Dmitry Boldyrev <ribkatt@gmail.com>
2017-02-20 02:58:27 +0300

    Distr resolve & typings
    Signed-off-by: Dmitry Boldyrev <ribkatt@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,19 @@
 declare module 'telegram-mtproto' {
   class ApiManager {
+    constructor()
+    constructor({ serverConfig: Object, appSettings: Object, schema: Object, debug: boolean } = {})
     mtpGetNetworker (dc: number): any
     mtpGetNetworker (dc: number, options: Object): any
+    mtpInvokeApi(method: string): Promise<Object>
+    mtpInvokeApi(method: string, params: Object): Promise<Object>
+    mtpInvokeApi(method: string, params: Object, options: Object): Promise<Object>
+    on(event: string|string[], handler: Function)
   }
   const api: ApiManager
+  interface AsyncStorage {
+    get: (...keys:string[]) => Promise<any[]>
+    set: (obj: Object) => Promise<Object>
+    remove: (...keys:string[]) => Promise<any>
+    clear: () => Promise<{}>
+  }
 }
