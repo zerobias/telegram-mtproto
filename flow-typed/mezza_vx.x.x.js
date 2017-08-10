b68332c95daeb30ffc894b0d8c109146e58c850f
Dmitry Boldyrev <ribkatt@gmail.com>
2017-08-10 13:05:08 +0300

    Remove unused worker-loader, funfix and mezza packages
    Signed-off-by: Dmitry Boldyrev <ribkatt@gmail.com>

--- flow-typed/mezza_vx.x.x.js
+++
@@ -1,3 +0,0 @@
-declare module 'mezza' {
-  declare export default function choose<Input, Match: { [key: string]: (val: Input) => boolean }>(match: Match, matcher: $ObjMap<Match, (val: Input) => any>): any
-}
