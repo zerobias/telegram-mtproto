84c2032dc10d294050662512f0bce17cca891d5a
Dmitry Boldyrev <ribkatt@gmail.com>
2018-02-22 13:30:13 +0300

    Remove early internal effector

--- packages/telegram-mtproto/src/util/action/index.js
+++
@@ -1,8 +0,0 @@
-//@flow
-
-export {Act} from './act'
-export {Action, action} from './action'
-export {AsyncAction} from './async-action'
-export {createReducer, ReduxReducer} from './create-reducer'
-
-export {actionMiddleware} from './middleware'
